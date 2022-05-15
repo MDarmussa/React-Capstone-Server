2a1a4dfba5ec4d74d07a032ab56615f28f85bc9c
rashida-otunba <rashidaotunba@gmail.com>
2022-05-15 12:55:18 -0400

    server is working; updates to server.js; bodyParser installed

--- app/models/index.js
+++ app/models/index.js
@@ -4,6 +4,6 @@ mongoose.Promise = global.Promise;
 const db = {};
 db.mongoose = mongoose;
 db.url = dbConfig.url;
-db.tutorials = require("./tutorial.model.js")(mongoose);
+db.user = require("./user")(mongoose);
 module.exports = db;
  
