1dc02b5f4b142071221197ac3eb8a1595bdf37c0
Kelli <kellistone217@gmail.com>
2022-05-19 17:04:42 -0600

    Created backend routes for expenses

--- app/routes/expense.routes.js
+++ app/routes/expense.routes.js
@@ -1,23 +1,39 @@
 const express = require('express');
 const router = express.Router();
 const Expense = require('../models/expense');
-  
+
+// Create a new expense
   router.post("/create", async (req, res, next) => {
     const { type, amount, payment, date, comment } = req.body
     const createExpense = await Expense.create(req.body)
     res.json(createExpense)
   });
+
   // // Retrieve all expense
   // router.get("/:id", expense.findAll);
-  // // Retrieve a single expense with id
-  // router.get("/:id", expense.findOne);
-  // // Update a expense with id
-  // router.put("/:id", expense.update);
-  // // Delete a expense with id
-  // router.delete("/:id", expense.delete);
-  // // Create a new expense
-  // router.delete("/", expense.deleteAll);
-  // app.use("/api/expense", router);
+ 
+  // Retrieve a single expense with id
+  router.get('/:id', async function(req, res, next) {
+    const expense = await Expense.findById ({
+        _id: req.params.id });
+    res.json(expense);
+  });
+  
+  // Update a expense with id
+  router.patch('/:id', async function(req, res, next) {
+    const {amount, type, payment, date, comment} = req.body
+    const updateExpense = await Expense.findByIdAndUpdate (
+    req.params.id, req.body );
+    res.json(updateExpense);
+  });
+
+  // Delete a expense with id
+  router.delete('/:id', async function(req, res, next) {
+    const deleteExpense = await Expense.findByIdAndDelete (
+    req.params.id);
+    res.json(deleteExpense);
+  });
+  
 
 
 module.exports = router
